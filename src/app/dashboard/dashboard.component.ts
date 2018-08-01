b0a1cbbe9c5281e422f58f4344a5dc2388c0ad13
Matthew Clancy <matthewpclancy@mpc.local>
2018-08-01 13:53:45 -0400

    chartfix and routerlink to button on dash

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,21 +15,17 @@ export class DashboardComponent implements OnInit {
   public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType = 'bar';
   public barChartLegend = true;
- 
   public barChartData = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
   ];
- 
   // events
   public chartClicked(e: any): void {
     console.log(e);
   }
- 
   public chartHovered(e: any): void {
     console.log(e);
   }
- 
   public randomize(): void {
     // Only Change 3 values
     const data = [
