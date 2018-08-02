102b4a85a05a4f1659ed578b619be688f2c5fdce
Matthew Clancy <matthewpclancy@mpc.local>
2018-08-02 14:48:55 -0400

    fix randomize

--- src/app/token.service.ts
+++ src/app/token.service.ts
@@ -53,14 +53,7 @@ randomizeToken(token: Token): Observable<any> {
        (Math.random() * 100),
        (Math.random() * 100)]
       , label: 'Series A'},
-    {data: [(Math.random() * 100),
-       (Math.random() * 100),
-       (Math.random() * 100),
-       (Math.random() * 100),
-       (Math.random() * 100),
-       (Math.random() * 100),
-       (Math.random() * 100)]
-      , label: 'Series B'}
+      token.barData[1]
   ];
   return this.http.put(this.tokensUrl, token, httpOptions).pipe(
     tap(_ => this.log(`updated token id=${token.id}`)),
